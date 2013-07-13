01fe37a2e992e3912b9cb361b6f5f8e2f52a016c
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2013-07-13 15:33:40 +0300

    draw simple lines

--- DrawBotImageConverter/src/com/jff/arduino/drawbot/image/convertor/model/PaintLine.java
+++ DrawBotImageConverter/src/com/jff/arduino/drawbot/image/convertor/model/PaintLine.java
@@ -5,6 +5,14 @@ import java.util.List;
 
 public class PaintLine {
 
+    private static final double R_STEP = 20;
+    private static final double THETA_STEP = Math.PI / 2 * R_STEP;
+
+    private static final double CYLINDER_RADIUS = 50;
+    private static final double CYLINDER_STEPS = 200;
+
+    private static final double MIN_LINE_LENGTH_STEP = Math.PI * 2 * CYLINDER_RADIUS / CYLINDER_STEPS;
+
     private List<Point2D> points = new ArrayList<Point2D>();
 
     public List<Point2D> getPoints() {
@@ -24,29 +32,38 @@ public class PaintLine {
 
         double diagonalLength = (float) Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
 
-        double rStep = 5;
-        double thetaStep = 0.001;
+        double rStep = R_STEP;
 
-        for (double i = 0; i < diagonalLength; i += rStep) {
+        for (double i = 0; i <= diagonalLength; i += rStep) {
             double r = i;
 
+            double thetaStep = CYLINDER_RADIUS / r;
+
+            double theta;
+            double nextTheta;
 
             if (reverse) {
-                for (double j = 0; j <= Math.PI / 2; j += thetaStep) {
+                for (double j = 0; j < Math.PI / 2; j += thetaStep) {
 
-                    double theta = j;
+                    theta = j;
 
-                    addPointIntoLine(paintLine, r, theta, width, height);
+                    nextTheta = theta + thetaStep;
+                    nextTheta = Math.min(nextTheta, Math.PI);
 
+                    addPointIntoLine(grayScaleImageBitmap, paintLine, r, theta, nextTheta, width, height);
 
                 }
+
             } else {
 
-                for (double j = Math.PI / 2; j >= 0; j -= thetaStep) {
+                for (double j = Math.PI / 2; j > 0; j -= thetaStep) {
 
-                    double theta = j;
+                    theta = j;
 
-                    addPointIntoLine(paintLine, r, theta, width, height);
+                    nextTheta = theta - thetaStep;
+                    nextTheta = Math.max(nextTheta, 0);
+
+                    addPointIntoLine(grayScaleImageBitmap, paintLine, r, theta, nextTheta, width, height);
 
 
                 }
@@ -58,20 +75,78 @@ public class PaintLine {
 
         }
 
-
         return paintLine;
     }
 
-    private static void addPointIntoLine(PaintLine paintLine, double r, double theta, int width, int height) {
-        PolarPoint2D polarPoint2D = new PolarPoint2D(r, theta);
+    private static void addPointIntoLine(GrayScaleImageBitmap grayScaleImageBitmap,
+                                         PaintLine paintLine, double r, double theta,
+                                         double nextTheta, int width, int height) {
 
-        Point2D point2D = PolarPoint2D.toCartesian(polarPoint2D);
 
 
-        if (point2D.x < width && point2D.y < height) {
+//        PolarPoint2D currentPolarPoint2D = new PolarPoint2D(r, theta);
+//
+//        Point2D currentPoint2D = PolarPoint2D.toCartesian(currentPolarPoint2D);
 
-            paintLine.points.add(point2D);
-        }
+        int[][] bitmap = grayScaleImageBitmap.getBitmap();
+
+
+
+
+//            int gray = bitmap[currentPoint2D.x][currentPoint2D.y];
+
+
+            double thetaStep = nextTheta - theta;
+            double rStep = R_STEP;
+
+
+
+            double currentTheta = theta;
+
+
+            while(true) {
+
+                double minStep =  MIN_LINE_LENGTH_STEP/ r;
+
+                for(int i = 0; i < rStep; i++) {
+
+                    double currentR = r - i;
+
+
+
+
+                    PolarPoint2D innerPolarPoint2D = new PolarPoint2D(currentR, currentTheta);
+
+                    Point2D innerPoint2D = PolarPoint2D.toCartesian(innerPolarPoint2D);
+
+                    if (innerPoint2D.x < width && innerPoint2D.y < height) {
+
+                        paintLine.points.add(innerPoint2D);
+
+                    }
+
+                }
+
+                if(theta < nextTheta) {
+
+                    currentTheta += minStep;
+                    if(currentTheta > nextTheta) {
+                        break;
+                    }
+                } else {
+                    currentTheta -= minStep;
+                    if(currentTheta < nextTheta) {
+                        break;
+                    }
+                }
+
+            }
+
+
+
+
+
+
 
 
     }
--- DrawBotImageConverter/src/com/jff/arduino/drawbot/image/convertor/model/Point2D.java
+++ DrawBotImageConverter/src/com/jff/arduino/drawbot/image/convertor/model/Point2D.java
@@ -13,4 +13,33 @@ public class Point2D {
 
 
     }
+
+    @Override
+    public String toString() {
+        return "Point2D{" +
+                "x=" + x +
+                ", y=" + y +
+                "} ";
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point2D point2D = (Point2D) o;
+
+        if (x != point2D.x) return false;
+        if (y != point2D.y) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
